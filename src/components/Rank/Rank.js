5754df4ac5d8729493432349d3f463c2b83640d2
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-06-25 17:30:38 +0530

    components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+const Rank = () =>{
+    return(
+        <div>
+        <div className='white f3 '>
+          {'Aditya, your current rank is ....'}
+        </div>
+        <div className='white f1 '>
+          {'#5'}
+        </div>
+        
+        </div>
+        
+            
+        
+       
+    );
+}
+
+
+export default Rank;
