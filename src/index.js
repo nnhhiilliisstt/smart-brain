2c030865a80cd0bb6da7a239fce4d33f403fbd6a
8591777904abhinav <63181611+8591777904abhinav@users.noreply.github.com>
2020-07-13 20:24:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
+import.log('index.html')
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
