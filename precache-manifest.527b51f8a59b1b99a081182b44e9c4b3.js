2ec361761c5c54fab6f56f836d6ab7089934348f
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-07-31 03:17:25 +0530

    Updates

---
+++ precache-manifest.527b51f8a59b1b99a081182b44e9c4b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e400af8a631d60d3389e5d543a65b08",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "40ac5ba96dec572b4abe",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9b4161d61bc4a7b1002a",
+    "url": "/smart-brain/static/css/main.735c2bff.chunk.css"
+  },
+  {
+    "revision": "40ac5ba96dec572b4abe",
+    "url": "/smart-brain/static/js/2.09f37d24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.09f37d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4161d61bc4a7b1002a",
+    "url": "/smart-brain/static/js/main.1a288277.chunk.js"
+  },
+  {
+    "revision": "5f284a9b7132a669a655",
+    "url": "/smart-brain/static/js/runtime-main.43daafc1.js"
+  }
+]);
